66f21184f05e0ed8b00c990d1787672b137a6e5b
La Prise <gaurondorian@gmail.com>
2021-03-19 10:00:11 +0100

    tout dans une seule fonction

--- public/script/home/rapport.js
+++ public/script/home/rapport.js
@@ -2,8 +2,7 @@ const table_rapport = document.querySelector('#table-rapport')
 const table_utilisateur = document.querySelector('#table-utilisateur')
 const table_produit = document.querySelector('#table-produit-reference')
 
-Myrequest("/API/select/all_rapport","GET")
-.then((data)=>{
+const feed_table = (table,data) =>{
     for (const key in data) {
         const tr = document.createElement('tr')
         for (const [keys, value] of Object.entries(data[key])) {
@@ -13,39 +12,15 @@ Myrequest("/API/select/all_rapport","GET")
             tr.appendChild(td)
         }
         
-        table_rapport.appendChild(tr)
+        table.appendChild(tr)
     }
-})
+}
+
+Myrequest("/API/select/all_rapport","GET")
+.then((data)=> feed_table(table_rapport,data) )
 
 Myrequest("/API/select/all_controleur","GET")
-.then((data)=>{
-    for (const key in data) {
-        const tr = document.createElement('tr')
-        for (const [keys, value] of Object.entries(data[key])) {
-            const td = document.createElement('td')
-            const text_node = document.createTextNode(`${value}`)
-            td.appendChild(text_node)
-            tr.appendChild(td)
-        }
-        
-        table_utilisateur.appendChild(tr)
-    }
-    console.log(data)
-})
-
+.then((data)=> feed_table(table_utilisateur,data) )
 
 Myrequest("/API/select/all_product","GET")
-.then((data)=>{
-    for (const key in data) {
-        const tr = document.createElement('tr')
-        for (const [keys, value] of Object.entries(data[key])) {
-            const td = document.createElement('td')
-            const text_node = document.createTextNode(`${value}`)
-            td.appendChild(text_node)
-            tr.appendChild(td)
-        }
-        
-        table_produit.appendChild(tr)
-    }
-    console.log(data)
-})
+.then((data)=> feed_table(table_produit,data) )
