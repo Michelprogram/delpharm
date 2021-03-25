40aed0893c22b1ab526aad7ce1f9f1908a77b22e
La Prise <gaurondorian@gmail.com>
2021-03-25 16:40:08 +0100

    changement id en class button_container

--- public/script/home/poids.js
+++ public/script/home/poids.js
@@ -2,17 +2,28 @@ const span_poids = document.querySelector('#Poids_produit')
 const span_variation = document.querySelector('#Variation')
 
 const conforme = document.querySelector('#Conforme')
+const img_button = document.querySelector('#img-button-peser')
+const text_button_peser = document.querySelector('#texte-peser')
 
 const ref_produit = document.querySelector('#ref-produit')
 const peser_button = document.querySelector('.peser-button')
 
 const nb_produit = document.querySelector('#nb_produit')
 
+
+const animation_button = () =>{
+    text_button_peser.innerHTML = ""
+    img_button.setAttribute('src','/static/images/home/load.svg')
+}
+
+
 peser_button.addEventListener('click',(e)=>{
     const data = {
         nom_produit: ref_produit.value,
         nombre_de_produit: nb_produit.value
     }
+
+    animation_button()
     
     Myrequest("/API/balance/weight","POST",data) 
     .then((data)=>{
