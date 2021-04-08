63f4b30e3115119a1cf652b2c4c555bc0d257f4e
gaurondorian <gaurondorian@gamil.com>
2021-04-08 10:02:05 +0200

    animation refresh du tableaux

--- public/JS/home/tableaux.js
+++ public/JS/home/tableaux.js
@@ -1,13 +1,19 @@
 //Gestion des tableaux Rapport, Produit, Référence
 class Tableau{
 
-    constructor(){}
+    constructor(){
+        this.table_rapport = document.querySelector('#table-rapport')
+        this.table_produit = document.querySelector('#table-produit-reference')
+        this.table_controleur = document.querySelector('#table-utilisateur')
+
+
+    }
 
     _clean_table(table){
         const nb_rows = table.rows.length
         //Commence à 1 pour pas effacer les th
         for(let i=1;i<nb_rows;i++){
-            table.rows[i].innerHTML = ""
+            table.removeChild(table.children[1])
         }
     }
 
@@ -15,6 +21,7 @@ class Tableau{
         this._clean_table(table)
         for (const key in data) {
             const tr = document.createElement('tr')
+            tr.setAttribute('class','data-from-BDD')
             for (const [keys, value] of Object.entries(data[key])) {
                 const td = document.createElement('td')
                 const text_node = document.createTextNode(`${value}`)
@@ -26,34 +33,31 @@ class Tableau{
     }
 
     rapport = function(){
-        const table_rapport = document.querySelector('#table-rapport')
         Myrequest("/API/select/all_rapport","GET")
         .then((data)=>{
             //Supprime T et Z dans les dates format ISO 8601
             data.forEach(element => element.Time = element.Time.replace(/T|Z/gm,' '))
-            this._remplir_tableau(table_rapport,data)
+            this._remplir_tableau(this.table_rapport,data)
         })
     }
 
     produit (){
-        const table_produit = document.querySelector('#table-produit-reference')
         Myrequest("/API/select/all_product","GET")
-        .then((data)=> this._remplir_tableau(table_produit,data) )
+        .then((data)=> this._remplir_tableau(this.table_produit,data) )
     }
 
     controleur (){
-        const table_controleur = document.querySelector('#table-utilisateur')
-
         Myrequest("/API/select/all_controleur","GET")
-        .then((data)=> this._remplir_tableau(table_controleur,data))
+        .then((data)=> this._remplir_tableau(this.table_controleur,data))
     }
     
 }
 
+
 const button_refresh = document.querySelectorAll('.refresh-button')
 const animation_button_refresh = (img)=>{
     img.setAttribute('class','refresh-button-img-trigger')
-    setTimeout(()=>img.setAttribute('class','refresh-button-img'),500)
+    setTimeout(()=>{img.setAttribute('class','refresh-button-img')},500)
 }
 
 const tables = new Tableau()
@@ -63,8 +67,7 @@ tables.controleur()
 
 button_refresh.forEach((button)=>{
     button.addEventListener('click',(e)=>{
-        const target = e.target
-        target.setAttribute('class','refresh-button-img-trigger')
+        animation_button_refresh(e.target)
         const id = e.target.parentNode.id.slice(-1)
         switch(id){
             case "1":
